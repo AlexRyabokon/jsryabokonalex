73a7e3e86601712dc53c8a54797dacc7477c98e1
Alexander Ryabokon <leonid555100@gmail.com>
2021-11-09 22:30:57 +0200

    final homework 3

--- homework_3/index.js
+++ homework_3/index.js
@@ -1,1 +1,44 @@
-cccccccccccccccccccccccc 00010101010001111001111
+/* - Написать функцию filterBy(), которая будет принимать
+ в себя 2 аргумента. Первый аргумент - массив, который будет 
+ содержать в себе любые данные, второй аргумент - тип данных. 
+ 
+ - Функция должна вернуть новый массив, который будет содержать 
+ в себе все данные, которые были переданы в аргумент, за 
+ исключением тех, тип которых был передан вторым аргументом. 
+То есть, если передать массив ['hello', 'world', 23, '23', null],
+ и вторым аргументом передать 'string', то функция вернет массив [23, null].*/
+
+
+//author - Alexander Ryabokon
+
+// first solution
+
+
+let arr = ['Vasya', 'Petya', 'Uchat', 'JS', 109, 55, 99, null,]; 
+
+function filterBy(arr, type) { 
+    return arr.filter(item => typeof item !== type);
+ } 
+ 
+ console.log(filterBy(arr, "string")); 
+ console.log(filterBy(arr, "number"));
+
+
+// second solution - delete comment symbols for examination
+
+/*
+
+let item1 = prompt('Put first item in array', '');
+    item2 = prompt('Put second item in array', '');
+    item3 = +prompt('Put the number in array', '');
+
+let arr2 = [item1, item2, item3];
+
+
+function filterBy(arr2, type) { 
+  return arr2.filter(item => typeof item !== type);
+} 
+
+console.log(filterBy(arr2, "string")); 
+console.log(filterBy(arr2, "number")); 
+*/
